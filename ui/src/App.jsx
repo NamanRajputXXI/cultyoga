3f73e45b3a57390cf85ce0f6c7e2a1390a7adb9d
Naman Rajput <namanrajput801@gmail.com>
2024-08-15 23:42:50 +0530

    ui:commit for the testing purpose

--- ui/src/App.jsx
+++ ui/src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const App = () => {
-  return <div>hello this is the cult yoga website</div>;
+  return <div>hello this is the cult yoga </div>;
 };
 
 export default App;
