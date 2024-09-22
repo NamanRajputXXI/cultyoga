91becb91f5028e77e6896224c75307b970c2d8b1
Naman Rajput <namanrajput801@gmail.com>
2024-09-22 21:44:38 +0530

    ui:setup tailwind and make the ui of login page

--- ui/src/pages/Home.jsx
+++ ui/src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Home = () => {
-  return <div>this is the home page</div>;
+  return <div className="text-2xl">this is the home page</div>;
 };
 
 export default Home;
