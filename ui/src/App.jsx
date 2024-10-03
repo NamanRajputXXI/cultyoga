1155ad2d41cd17b777c9d46b62ddb5e6a6c8d278
Naman Rajput <namanrajput801@gmail.com>
2024-10-03 21:49:16 +0530

    fix:build error

--- ui/src/App.jsx
+++ ui/src/App.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import Test from "./components/Test"; // Assuming you want to keep this component
 import AllRoutes from "./AllRoutes"; // Import your AllRoutes component
 
 const App = () => {
