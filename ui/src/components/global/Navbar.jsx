5cd1ceef9a6345e07138eaa569de7c279b3dac2a
Naman Rajput <namanrajput801@gmail.com>
2024-10-06 22:48:25 +0530

    ui:changes in nav, remove iconify install react-icons

--- ui/src/components/global/Navbar.jsx
+++ ui/src/components/global/Navbar.jsx
@@ -1,76 +1,85 @@
-import React, { useEffect, useRef, useState } from 'react'
-import { Icon } from '@iconify/react';
-
+import React, { useEffect, useRef, useState } from "react";
+import { RxHamburgerMenu, RxCross1 } from "react-icons/rx";
 
 const Navbar = () => {
-  const [showMenu, setShowMenu] = useState('false')
+  const [showMenu, setShowMenu] = useState("false");
   const menuRef = useRef(null);
 
   const showMenuFunction = () => {
-    setShowMenu('true')
-  }
+    setShowMenu("true");
+  };
 
   const hideMenuFunction = () => {
-    setShowMenu('false')
-  }
+    setShowMenu("false");
+  };
 
   const handleClickOutside = (event) => {
     if (menuRef.current && !menuRef.current.contains(event.target)) {
       hideMenuFunction();
+      console.log(menuRef.current);
     }
   };
 
   useEffect(() => {
-    document.addEventListener('click', handleClickOutside, true);
+    console.log("useEfffect is running");
+
+    document.addEventListener("click", handleClickOutside, true);
     return () => {
-      document.removeEventListener('click', handleClickOutside, true);
+      document.removeEventListener("click", handleClickOutside, true);
     };
   }, []);
 
-
   return (
-    <nav className='h-24 w-full'>
-      <div className='px-[5%] flex justify-between items-center h-full'>
+    <nav className=" w-full">
+      <div className="p-5 flex justify-between items-center h-full">
         <div>
-          <h1 className='font-bold text-3xl'>CultYog</h1>
+          <h1 className="font-bold text-3xl">CultYoga</h1>
         </div>
-        <ul className='hidden md:flex gap-10'>
-          <li className='font-semibold cursor-pointer'>Home</li>
-          <li className='font-semibold cursor-pointer'>About</li>
-          <li className='font-semibold cursor-pointer'>Services</li>
-          <li className='font-semibold cursor-pointer'>Products</li>
-          <li className='font-semibold cursor-pointer'>Contact Us</li>
+        <ul className="hidden md:flex gap-10">
+          <li className="font-semibold cursor-pointer">Home</li>
+          <li className="font-semibold cursor-pointer">About</li>
+          <li className="font-semibold cursor-pointer">Services</li>
+          <li className="font-semibold cursor-pointer">Products</li>
+          <li className="font-semibold cursor-pointer">Contact Us</li>
         </ul>
-        <div className='hidden md:flex'>
-          <button className='text-xl text-white font-semibold bg-green-500 px-5 py-1 rounded-md'>Log In</button>
+        <div className="hidden md:flex">
+          <button className="text-xl text-white font-semibold bg-green-500 px-5 py-1 rounded-md">
+            Log In
+          </button>
         </div>
-        <div onClick={showMenuFunction} className='cursor-pointer  md:hidden'>
-          <Icon icon="icon-park:hamburger-button" className='text-4xl' />
+        <div onClick={showMenuFunction} className="cursor-pointer  md:hidden">
+          <RxHamburgerMenu className="text-4xl" />
         </div>
       </div>
 
       {/* -------------------------Mobile Menu Slide-------------------------- */}
-      <div ref={menuRef} className={`bg-gray-300 p-6 fixed w-64 h-screen flex flex-col gap-8 top-0 transform transition-transform duration-500 ease-out ${showMenu == 'true' ? 'translate-x-0' : '-translate-x-full'}`}>
-        <div onClick={hideMenuFunction} className='absolute right-0 top-3 cursor-pointer'>
-          <Icon icon="charm:cross" className='text-6xl' />
+      <div
+        ref={menuRef}
+        className={`bg-gray-300 p-5 fixed w-64 h-screen flex flex-col gap-8 top-0 transform transition-transform duration-500 ease-out ${
+          showMenu == "true" ? "translate-x-0" : "-translate-x-full"
+        }`}
+      >
+        <div className="flex justify-between w-full">
+          <h1 className="font-bold text-3xl">CultYoga</h1>
+          <div onClick={hideMenuFunction} className=" cursor-pointer">
+            <RxCross1 className="text-4xl" />
+          </div>
         </div>
-        <div>
-          <h1 className='font-bold text-3xl'>CultYog</h1>
-        </div>
-        <ul className='flex flex-col gap-4'>
-          <li className='font-semibold cursor-pointer'>Home</li>
-          <li className='font-semibold cursor-pointer'>About</li>
-          <li className='font-semibold cursor-pointer'>Services</li>
-          <li className='font-semibold cursor-pointer'>Products</li>
-          <li className='font-semibold cursor-pointer'>Contact Us</li>
+        <ul className="flex flex-col gap-8">
+          <li className="font-semibold cursor-pointer">Home</li>
+          <li className="font-semibold cursor-pointer">About</li>
+          <li className="font-semibold cursor-pointer">Services</li>
+          <li className="font-semibold cursor-pointer">Products</li>
+          <li className="font-semibold cursor-pointer">Contact Us</li>
         </ul>
         <div>
-          <button className='text-xl text-white font-semibold bg-green-500 px-5 py-2 rounded-sm'>Log In</button>
+          <button className="text-xl text-white font-semibold bg-green-500 px-5 py-2 rounded-sm">
+            Log In
+          </button>
         </div>
       </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
-
+export default Navbar;
