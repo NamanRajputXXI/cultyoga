8e77e2581deea0bb5f948ddd06fddd649805236c
Naman Rajput <namanrajput801@gmail.com>
2024-10-25 16:58:17 +0530

    feat:add highlighted option and keyboard navigation functionality

--- ui/src/components/home/SearchBar.jsx
+++ ui/src/components/home/SearchBar.jsx
@@ -5,6 +5,8 @@ const SearchBar = () => {
   const [input, setInput] = useState("");
   const [data, setData] = useState([]);
   const [filterData, setFilterData] = useState([]);
+  const [highlightedIndex, setHighlightedIndex] = useState(-1);
+  const [searchPerformed, setSearchPerformed] = useState(false);
   const inputRef = useRef(null);
 
   // click function for the search button
@@ -19,12 +21,14 @@ const SearchBar = () => {
     setInput(e.target.value);
     console.log(input);
     if (e.target.value === "") {
+      setHighlightedIndex(-1);
       setFilterData([]);
     } else {
       const filteredData = data.filter((item, i) =>
-        item.name.toLowerCase().includes(e.target.value.toLowerCase())
+        item.name.toLowerCase().startsWith(e.target.value.toLowerCase())
       );
       setFilterData(filteredData);
+      setSearchPerformed(true);
     }
   };
 
@@ -37,6 +41,29 @@ const SearchBar = () => {
       console.log(data);
     } catch (err) {
       console.log("Not able to fetch", err);
+    }
+  };
+
+  // handle key down for the keyboard navigation
+  const handleKeyDown = (e) => {
+    if (e.key === "ArrowDown") {
+      e.preventDefault(); // Prevent scrolling
+      setHighlightedIndex((prevIndex) =>
+        prevIndex < filterData.length - 1 ? prevIndex + 1 : prevIndex
+      );
+    } else if (e.key === "ArrowUp") {
+      e.preventDefault(); // Prevent scrolling
+      setHighlightedIndex((prevIndex) =>
+        prevIndex > 0 ? prevIndex - 1 : prevIndex
+      );
+    } else if (e.key === "Enter") {
+      if (highlightedIndex >= 0) {
+        const selectedItem = filterData[highlightedIndex];
+        setInput(selectedItem.name); // Set input to the selected item's name
+        setFilterData([]); // Clear the filtered data
+        setHighlightedIndex(-1); // Reset highlighted index
+        setSearchPerformed(false); // when hit enter it means search is performed
+      }
     }
   };
 
@@ -53,20 +80,39 @@ const SearchBar = () => {
         </div>
         <input
           onChange={handleChange}
+          onKeyDown={handleKeyDown}
           ref={inputRef}
           type="text"
           value={input}
           placeholder="Search for yoga poses or disease"
-          className="w-full sm:h-12 h-10  border-gray-300 rounded-xl  pl-10 sm:px-12  focus:outline-none" // Add padding to left with px-12
+          className={`w-full sm:h-12 h-10  border-gray-300 ${
+            filterData.length === 0 ? "rounded-xl" : "rounded-t-xl"
+          }  pl-10 sm:px-12  focus:outline-none`}
         />
       </div>
-      {filterData.map((item, i) => {
-        return (
-          <div className="w-full rounded-xl px-12 py-2 bg-white">
-            <p>{item.name}</p>
-          </div>
-        );
-      })}
+      {filterData.length > 0
+        ? filterData.map((item, i) => (
+            <div
+              key={i} // Adding a unique key is important for list items
+              className={`w-full mt-2 h-fit absolute top-10 rounded-b-xl px-12 py-2 bg-white ${
+                highlightedIndex === i ? "bg-gray-200" : ""
+              }`}
+              onMouseEnter={() => setHighlightedIndex(i)} // Highlight on mouse hover
+              onClick={() => {
+                setInput(item.name); // Set input to the selected item's name
+                setFilterData([]); // Clear the filtered data
+                setHighlightedIndex(-1); // Reset highlighted index
+              }}
+            >
+              <p>{item.name}</p>
+            </div>
+          ))
+        : input !== "" &&
+          searchPerformed && (
+            <div className="w-full  h-fit absolute top-10 rounded-b-xl px-12 py-2 bg-white">
+              <p>No data found</p>
+            </div>
+          )}
     </div>
   );
 };
